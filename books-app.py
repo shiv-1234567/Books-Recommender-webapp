938a8090b497d8ecea7f336a07ec9ac8d2afc61e
Shivendra prasad Mishra <shivendramishra346@gmail.com>
2024-07-09 01:09:03 +0530

    Update books-app.py

--- books-app.py
+++ books-app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 popular_df = pickle.load(open('popular.pkl','rb'))
 pt = pickle.load(open('pt.pkl','rb'))
-books = pickle.load(open('books.pkl','rb'))
+# books = pickle.load(open('books.pkl','rb'))
 similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 
 app = Flask(__name__)
